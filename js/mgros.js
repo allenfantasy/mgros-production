b1ea878dbe23c378b482d80e2cca2da2c1c1ec98
Allen Wu <allenwu1018@gmail.com>
2013-03-11 14:56:10 +0800

    switch image to pure color when hovered; image/color switcher

--- js/mgros.js
+++ js/mgros.js
@@ -1,12 +1,13 @@
 // Custom Javascript by Allen( dev.cna@borsch.net )
-/*(function($){
-	$(document).ready(function() {
+(function($){
+  $(document).ready(function() {
     $('.view-header-topics .views-row img').hover(
       function(e){
-        $(e.target).hide();
-      },
-      function(e){
-        $(e.target).show();
-      }
-    );
-})(jQuery);*/
+      //$(e.target).css('visibility', 'hidden');
+      $(e.target).hide();
+	});
+    $('.view-header-topics .views-row').mouseleave(function(e) {
+      $(e.target).find('img').show();
+    })	
+  });
+})(jQuery);
--- views-view--header-topics.tpl.php
+++ views-view--header-topics.tpl.php
@@ -3,6 +3,8 @@ for($i=0;$i<count($variables['view']->style_plugin->rendered_fields);$i++) {
   $info[$i]['title'] = $variables['view']->style_plugin->rendered_fields[$i]['title'];
   $info[$i]['image'] = $variables['view']->style_plugin->rendered_fields[$i]['field_image'];
   $info[$i]['url'] = $variables['view']->style_plugin->rendered_fields[$i]['field_url'];
+  //$info[$i]['photo'] = ($variables['view']->style_plugin->rendered_fields[$i]['field_switch'] == 'photo') ;
+  $info[$i]['photo'] = $variables['view']->style_plugin->rendered_fields[$i]['field_switch'] == "Photo" ;
 }
 ?>
 <div class="view-header-topics">
@@ -12,16 +14,20 @@ for($i=0;$i<count($variables['view']->style_plugin->rendered_fields);$i++) {
     <div class="views-row span1">
         <?php $url = base_path() . $item['url']; ?>
         <a class="title" href="<?php print $url; ?>">
-          <?php if($item['image']) { ?>
+          <?php if($item['image'] && $item['photo']) { ?>
             <div class="title color-<?php print $i ?>">
           <?php } else { ?>
-            <div class="title">
+            <div class="title ">
           <?php } ?>
               <span class="inner"><?php print $item['title']; ?></span>
         </div></a>
       <a href="<?php print $url; ?>">
       <div class="image color-<?php print $i ?>">
-        <?php print $item['image']; ?>
+        <?php 
+          if ($item['photo']) { 
+            print $item['image']; 
+          } 
+        ?>
       </div></a>
     </div>
     <?php $i++; } ?>
